1ff1e59d43b698738feb7868026286113f54674e
iapYang <yklorse@gmail.com>
2017-02-14 17:32:15 +0800

    0214

--- learn.py
+++ learn.py
@@ -1,4 +1,11 @@
-a, b = 0, 1
-while b < 1000:
-    print(b)
-    a, b = b, a+b
+x = int(input("Please enter an integer: "))
+
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')

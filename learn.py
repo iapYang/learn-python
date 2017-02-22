08571fc5b8b386974f665a902f758fe15a572a28
iapYang <yklorse@gmail.com>
2017-02-22 17:34:21 +0800

    0222 unable to finish chart 4

--- learn.py
+++ learn.py
@@ -1,11 +1,13 @@
-x = int(input("Please enter an integer: "))
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
 
-if x < 0:
-    x = 0
-    print('Negative changed to zero')
-elif x == 0:
-    print('Zero')
-elif x == 1:
-    print('Single')
-else:
-    print('More')
+ask_ok('xxxx')

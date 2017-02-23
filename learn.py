3e4b5b9b1abd5a553a2f01108075f84b22bdf7f1
iapYang <yklorse@gmail.com>
2017-02-23 16:25:47 +0800

    0223 function use

--- learn.py
+++ learn.py
@@ -1,13 +1,3 @@
-def ask_ok(prompt, retries=4, reminder='Please try again!'):
-    while True:
-        ok = input(prompt)
-        if ok in ('y', 'ye', 'yes'):
-            return True
-        if ok in ('n', 'no', 'nop', 'nope'):
-            return False
-        retries = retries - 1
-        if retries < 0:
-            raise ValueError('invalid user response')
-        print(reminder)
-
-ask_ok('xxxx')
+def add(n):
+    return n+1
+print(add(10))

1f821ad3f494bb54dcd0ba36c4f5a88864d475dd
iapYang <yklorse@gmail.com>
2017-03-01 17:33:55 +0800

    0301 *args

--- learn.py
+++ learn.py
@@ -1,14 +1,5 @@
-def cheeseshop(kind, *arguments, **keywords):
-    print("-- Do you have any", kind, "?")
-    print("-- I'm sorry, we're all out of", kind)
-    for arg in arguments:
-        print(arg)
-    print("-" * 40)
-    for kw in keywords:
-        print(kw, ":", keywords[kw])
-
-cheeseshop("Limburger", "It's very runny, sir.",
-           "It's really very, VERY runny, sir.",
-           shopkeeper="Michael Palin",
-           client="John Cleese",
-           sketch="Cheese Shop Sketch")
+def concat(*args, sep="/"):
+    print(sep.join(args))
+    return sep.join(args)
+ 
+concat('xxx', 'wejthriuwtieryi',sep=".")

7dff02d323211a86655452222ea3d2d714580daa
iapYang <yklorse@gmail.com>
2017-03-01 17:39:57 +0800

    0301 匿名函数

--- learn.py
+++ learn.py
@@ -1,5 +1,8 @@
-def concat(*args, sep="/"):
-    print(sep.join(args))
-    return sep.join(args)
- 
-concat('xxx', 'wejthriuwtieryi',sep=".")
+def make_incrementor(n):
+    answer = lambda x: x + n
+    return answer
+
+f = make_incrementor(42)
+print(f(0))
+f(0)
+f(1)

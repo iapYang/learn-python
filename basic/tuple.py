ccfa70dd0462d63ada795732727a834ec08a3b66
iapyang <yklorse@gmail.com>
2018-03-13 21:06:02 +0800

    add tuple

---
+++ basic/tuple.py
@@ -0,0 +1,8 @@
+# tuple类型，一旦设定就不能修改
+classmates = ('A', 'B', 'C')
+
+# 定义一个元素的tuple时需要加,
+singleTuple = (1,)
+wrongSingleTuple = (1)
+
+print('wrongSingleTuple', wrongSingleTuple, '\n', 'singleTuple', singleTuple)
--- learn.py
+++ learn.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-variableList = ['AS', 123, ['XXX']]
 
-# tuple类型，一旦设定就不能修改
 
 

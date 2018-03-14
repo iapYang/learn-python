52bd62b514e6ca4729bfb70675b106dd031ddfb7
iapyang <yklorse@gmail.com>
2018-03-14 21:01:11 +0800

    learn dict & set

---
+++ basic/dictset.py
@@ -0,0 +1,19 @@
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+
+print('Teller' in d)
+
+d.get('Teller', -1)
+
+d.pop('Bob')
+
+print(d)
+
+s = set([1, 2, 3, 66, 5])
+
+s.add(99)
+
+print(s)
+
+s.remove(99)
+
+print(s)

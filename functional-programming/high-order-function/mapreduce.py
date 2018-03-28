780e7b9ec1b5a5f3c489bf3157f18d75f20d40b3
iapyang <yklorse@gmail.com>
2018-03-28 21:16:05 +0800

    learn map & reduce

---
+++ functional-programming/high-order-function/mapreduce.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+from functools import reduce
+
+
+def f(x):
+    return x * x
+
+
+r = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+print(list(r))
+
+
+def fn(x, y):
+    return x * 10 + y
+
+
+print(reduce(fn, [1, 3, 5, 7, 9]))
+
+
+def normalize(name):
+    return name.upper()
+
+
+# 测试:
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+
+def prod(L):
+    return reduce(lambda x, y: x * y, L)
+
+
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
+
+
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '.': -1}
+
+
+def str2float(s):
+    nums = map(lambda x: DIGITS[x], s)
+    point = 0
+
+    def to_float(f, n):
+        nonlocal point
+        if n == -1:
+            point = 1
+            return f
+        if point == 0:
+            return f * 10 + n
+        else:
+            point *= 10
+            return f + n / point
+
+    return reduce(to_float, nums, 0.0)
+
+
+str2float('123.45')
+print('str2float(\'123.456\') =', str2float('123.456'))
+if abs(str2float('123.456') - 123.456) < 0.00001:
+    print('测试成功!')
+else:
+    print('测试失败!')

706c14617ded18d0b104cfbc8ce6bb170ec86e36
iapyang <yklorse@gmail.com>
2018-03-29 21:04:30 +0800

    learn sort

---
+++ functional-programming/high-order-function/sort.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env/python3
+
+print(sorted([36, 5, -12, 9, -21]))
+
+print(sorted([36, 5, -12, 9, -21], key=abs))
+
+print(sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True))
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+
+def by_name(t):
+    return t[0]
+
+
+L2 = sorted(L, key=by_name)
+print(L2)
+
+
+def by_score(t):
+    return t[1]
+
+
+L2 = sorted(L, key=by_score, reverse=True)
+print(L2)

9a7b82db8b9b7871373b84494c04c22f01cd986c
iapyang <yklorse@gmail.com>
2018-07-03 12:58:06 +0800

    learn multi-thread

---
+++ learn-processing-thread/learn-multi-thread.py
@@ -0,0 +1,57 @@
+#!usr/bin/env/python3
+# -*- coding: utf-8 -*-
+# created_at: 2018/7/3 下午12:54
+
+__author__ = 'IAPYANG'
+
+import time, threading
+
+
+# 新线程执行的代码:
+def loop():
+    print('thread %s is running...' % threading.current_thread().name)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+        time.sleep(1)
+    print('thread %s ended.' % threading.current_thread().name)
+
+
+print('thread %s is running...' % threading.current_thread().name)
+t = threading.Thread(target=loop, name='LoopThread')
+t.start()
+t.join()
+print('thread %s ended.' % threading.current_thread().name)
+
+# 假定这是你的银行存款:
+balance = 0
+lock = threading.Lock()
+
+
+def change_it(n):
+    # 先存后取，结果应该为0:
+    global balance
+    for i in range(100000):
+        # 先要获取锁:
+        lock.acquire()
+        try:
+            # 放心地改吧:
+            change_it(n)
+        finally:
+            # 改完了一定要释放锁:
+            lock.release()
+
+
+def run_thread(n):
+    for i in range(100000):
+        change_it(n)
+
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
